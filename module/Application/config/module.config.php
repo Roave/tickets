7861ce7ba1dae80bbc1f585c2ccecee064d1812e
malukenho <admin@phpse.net>
2015-01-29 04:19:36 -0300

    Remove template_map key from view_manager configuration

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -63,12 +63,6 @@ return [
         'doctype'                  => 'HTML5',
         'not_found_template'       => 'error/404',
         'exception_template'       => 'error/index',
-        'template_map' => [
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-            'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
-        ],
         'template_path_stack' => [
             __DIR__ . '/../view',
         ],
