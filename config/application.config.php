7a19c8bcab872383d5f7f0f5496dbd63aabb6a26
malukenho <admin@phpse.net>
2015-01-29 04:03:20 -0300

    Remove unnecessary module_paths key from application.config

--- config/application.config.php
+++ config/application.config.php
@@ -9,11 +9,6 @@ return [
     ],
 
     'module_listener_options' => [
-        'module_paths' => [
-            './module',
-            './vendor',
-        ],
-
         'config_glob_paths' => [
             'config/autoload/{,*.}{global,local}.php',
         ],
