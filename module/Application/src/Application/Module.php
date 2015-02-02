88a17d867c1f212e5f7c5f509e4816da35ff3f58
malukenho <admin@phpse.net>
2015-02-02 07:52:04 -0300

    Remove getAutoloaderConfig() method of Module.php and use composer autoload instead.

--- module/Application/src/Application/Module.php
+++ module/Application/src/Application/Module.php
@@ -24,17 +24,6 @@ class Module implements ConfigProviderInterface
 {
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
-    }
-
-    public function getAutoloaderConfig()
-    {
-        return [
-            'Zend\Loader\StandardAutoloader' => [
-                'namespaces' => [
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ],
-            ],
-        ];
+        return include __DIR__ . '/../../config/module.config.php';
     }
 }
