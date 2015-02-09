c64a09618a4d6cd6e3612a51ff8eb9c3184da32b
malukenho <admin@phpse.net>
2015-02-09 08:58:33 -0300

    Configuration for zfcuser

---
+++ config/autoload/zfcuser.global.php
@@ -0,0 +1,24 @@
+<?php
+
+use Zend\Db\Adapter\Adapter;
+
+$settings = [
+    'enable_registration'               => true,
+    'enable_username'                   => true,
+    'enable_display_name'               => true,
+    'auth_identity_fields'              => ['username', 'email'],
+    'login_after_registration'          => true,
+    'use_redirect_parameter_if_present' => true,
+    'login_redirect_route'              => 'zfcuser',
+    'logout_redirect_route'             => 'zfcuser/login',
+    'table_name'                        => 'user',
+];
+
+return [
+    'zfcuser' => $settings,
+    'service_manager' => [
+        'aliases' => [
+            'zfcuser_zend_db_adapter' => Adapter::class,
+        ],
+    ],
+];
