e67a504fb8b10a07fdcb7afc259df84bf6516b5b
malukenho <admin@phpse.net>
2015-02-09 17:57:58 -0300

    Join array configuration on ZFCUser configuration

--- config/autoload/zfcuser.global.php
+++ config/autoload/zfcuser.global.php
@@ -2,20 +2,18 @@
 
 use Zend\Db\Adapter\Adapter;
 
-$settings = [
-    'enable_registration'               => true,
-    'enable_username'                   => true,
-    'enable_display_name'               => true,
-    'auth_identity_fields'              => ['username', 'email'],
-    'login_after_registration'          => true,
-    'use_redirect_parameter_if_present' => true,
-    'login_redirect_route'              => 'zfcuser',
-    'logout_redirect_route'             => 'zfcuser/login',
-    'table_name'                        => 'user',
-];
-
 return [
-    'zfcuser' => $settings,
+    'zfcuser' => [
+        'enable_registration'               => true,
+        'enable_username'                   => true,
+        'enable_display_name'               => true,
+        'auth_identity_fields'              => ['username', 'email'],
+        'login_after_registration'          => true,
+        'use_redirect_parameter_if_present' => true,
+        'login_redirect_route'              => 'zfcuser',
+        'logout_redirect_route'             => 'zfcuser/login',
+        'table_name'                        => 'user',
+    ],
     'service_manager' => [
         'aliases' => [
             'zfcuser_zend_db_adapter' => Adapter::class,
